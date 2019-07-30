9e3eb74c7f92f6f4749d575110877f0d77ec23ba
Neil Ramsbottom <nramsbottom@hotmail.com>
2019-07-31 00:04:31 +0200

    WIP

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,41 @@
 
 app.controller('mainController', function($scope) {
-  $scope.fileName = "none";
+  	
+	$scope.fileName = "";
   
-  $scope.start = {
-	  hour: 0,
-	  minute: 1,
-	  second: 0
-  };
+	$scope.start = {
+		hour: 0,
+		minute: 0,
+		second: 0
+	};
+
+	$scope.end = {
+		hour: 0,
+		minute: 0,
+		second: 0
+	};
+
+	$scope.input = {
+		fileName: ""
+	};
+
+	$scope.output = {
+		fileName: ""
+	};	
     
-  $scope.getCommandLine = function() {
-	  	  
-	  return "ffmpeg -s " + $scope.start.hour.toString() + ":" + $scope.start.minute.toString() + " " + $scope.fileName;
-  }
+	$scope.getCommandLine = function() {
+		// ffmpeg -ss hh:mm:ss -i input.mp4 -c copy -t hh:mm:ss -bsf:a aac_adtstoasc output.mp4
+		return "ffmpeg" 
+		+ " -ss " 
+		+ $scope.start.hour.pad(2).toString()  + ":" + $scope.start.minute.pad(2).toString() + ":" + $scope.start.second.pad(2).toString() 
+		+ " -i " + $scope.input.fileName	
+		+ " -c copy"
+		+ " -t "
+		+ $scope.end.hour.pad(2).toString()  + ":" + $scope.end.minute.pad(2).toString() + ":" + $scope.end.second.pad(2).toString()
+		+ " -bsf:a aac_adtstoasc "
+		+ $scope.output.fileName
+		;
+	}
   
   $scope.copyToClipboard = function(selector) {
 	var copyTextarea = document.querySelector(selector);
--- utility.js
+++ utility.js
@@ -1,20 +1,5 @@
-
-
-
-/*
-window.ondrop = function (e) {
-    // don't replace document
-    e.preventDefault();
-    e.stopPropagation();
-
-    // capture file
-    var dt = e.dataTransfer;
-    var files = dt.files;
-
-    var file = files[0];
-console.log(e.target);
-console.log(dt);
-console.log(files);
-
+Number.prototype.pad = function(size) {
+    var s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+        return s;
 }
-*/
